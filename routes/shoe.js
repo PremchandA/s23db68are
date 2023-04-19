5df6bfeaa08928ef3b65320fe681a24284e6c5f0
PremchandA <123143572+PremchandA@users.noreply.github.com>
2023-04-19 01:21:04 -0500

    added update query

--- routes/shoe.js
+++ routes/shoe.js
@@ -7,5 +7,9 @@ router.get('/', Shoe_controllers.shoe_view_all_Page );
 
 router.get('/detail', Shoe_controllers.shoe_view_one_Page);
 /* GET create costume page */
-router.get('/create', Shoe_controlers.shoe_create_Page);
+router.get('/create', Shoe_controllers.shoe_create_Page);
+/* GET create update page */
+router.get('/update', Shoe_controllers.shoe_update_Page);
+/* GET delete costume page */
+router.get('/delete', Shoe_controllers.shoe_delete_Page);
 module.exports = router;
