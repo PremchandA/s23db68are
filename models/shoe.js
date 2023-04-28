6be47de6fd3fd3fa49bc6ac4bb99b3e59d0ebaca
PremchandA <123143572+PremchandA@users.noreply.github.com>
2023-04-27 22:19:01 -0500

    added updare error

--- models/shoe.js
+++ models/shoe.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 const shoeSchema = mongoose.Schema({
     shoeName: String,
-    shoePrice: Number,
-    shoemadein: String
+    shoePrice:{type: Number,
+    min: 0,
+    max:500,
+     required: true},
+    shoemadein:{type: String}
 })
 module.exports = mongoose.model("shoe", shoeSchema)
